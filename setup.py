3196a27d88ab690edb01c640958dc56c1def5db3
Yves Martin <59341242+yvesmartindestaillades@users.noreply.github.com>
2023-03-29 12:05:03 -0700

    v 0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ requirements = open('requirements.txt').read().splitlines()
 
 setup(
     name='dms_ci',
-    version='0.1.0',
+    version='0.1.1',
     url='https://github.com/rouskinlab/DMS_CI',
     author='Yves Martin',
     author_email='yves@martin.yt',
