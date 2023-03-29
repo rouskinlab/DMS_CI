aaae3e684891fde436eda08666cc505f433b5008
Yves Martin des Taillades <59341242+yvesmartindestaillades@users.noreply.github.com>
2023-03-29 12:03:04 -0700

    Update dms_ci.py

--- dms_ci/dms_ci.py
+++ dms_ci/dms_ci.py
@@ -1,6 +1,6 @@
 from .src import wilson
 
-def dms_ci(p, n, z = 1.96, bias_correction=1E-3):
+def dms_ci(p, n, z = 1.96, sub_error=1E-3):
     """Provides confidence intervals for DMS-MaPseq data.
 
     Parameters
@@ -14,6 +14,9 @@ def dms_ci(p, n, z = 1.96, bias_correction=1E-3):
 
     alpha : float, optional
         Significance level of the confidence interval. Default is 0.05.
+        
+    sub_error: float, optional
+        Probability of having a substitution error in the sequencing. Used to unbias the CI. Default is 1E-3. Read the README for more details.
 
     Returns
     -------
@@ -27,9 +30,7 @@ def dms_ci(p, n, z = 1.96, bias_correction=1E-3):
     Notes
     -----
 
-    The confidence intervals are calculated using the Wilson score interval. 
-    The bias correction balances for the substitution errors in the sequencing. 
-
+    The confidence intervals are calculated using the Wilson score interval with a bias correction. 
     """
     
     return wilson(p, n, z, bias_correction)
